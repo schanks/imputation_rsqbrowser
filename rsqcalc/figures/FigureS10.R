27a7f5b7957cea16470ff1f94e254be2a0e6b4f6
Sarah Hanks <schanks@1000g.csgstat.sph.umich.edu>
2022-06-19 18:18:57 -0400

    Fixed legend spacing for Figures S1,S11,S12

--- rsqcalc/figures/FigureS10.R
+++ rsqcalc/figures/FigureS10.R
@@ -21,8 +21,10 @@ all$Ancestry=factor(all$Ancestry, levels=c("African","Hispanic/Latino","European
 ybrks=c(1,2,3,4,5,6)
 ylabs=c("Distance to\narray marker","GC content","Recombination\nrate","Repeats","Segmental\nduplication","Structural\nvariants")
 
+all$Parameter=tolower(all$Parameter)
+
 tiff(file="~/FigS10.tiff", height=174, width=114, units="mm", res=300)
-ggplot(all, aes(y=yval))+geom_vline(xintercept=0)+geom_point(aes(x=Beta, col=Ancestry))+geom_segment(aes(y=yval, yend=yval, x=Beta-1.96*SE, xend=Beta+1.96*SE, col=Ancestry))+facet_grid(Parameter~., scales="free")+scale_y_continuous(breaks=ybrks, labels=ylabs)+theme_bw()+scale_color_manual(values=c("#e41a1c","#4daf4a","#377eb8","#984ea3"))+theme(legend.text=element_text(size=7), legend.title=element_text(size=8), strip.text=element_text(size=8), axis.title=element_text(size=8), axis.text=element_text(size=7),panel.grid.minor=element_blank(),strip.background=element_rect(fill="white",color=NA))+ylab("Predictor")
+ggplot(all, aes(y=yval))+geom_vline(xintercept=0)+geom_point(aes(x=Beta, col=Ancestry))+geom_segment(aes(y=yval, yend=yval, x=Beta-1.96*SE, xend=Beta+1.96*SE, col=Ancestry))+facet_grid(Parameter~., scales="free", labeller=label_parsed)+scale_y_continuous(breaks=ybrks, labels=ylabs)+theme_bw()+scale_color_manual(values=c("#e41a1c","#4daf4a","#377eb8","#984ea3"))+theme(legend.text=element_text(size=7), legend.title=element_text(size=8), strip.text=element_text(size=8), axis.title=element_text(size=8), axis.text=element_text(size=7),panel.grid.minor=element_blank(),strip.background=element_rect(fill="white",color=NA))+ylab("Predictor")
 dev.off()
 
 
